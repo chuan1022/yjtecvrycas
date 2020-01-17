fd8b4fdf9c9568a6bd537f7167159cd5b3b83df3
yuchuan <825077792@qq.com>
2020-01-17 17:40:59 +0800

    uri

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,10 @@
 import { stringify, parse } from "qs";
 import cookie from "js-cookie";
 const key = "ticket";
-
+const default_redirect_uri = window.location.origin + window.location.pathname;
 let casUrl = "http://192.168.1.9:8000";
+console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV == "production") {
   casUrl = "http://cas.jsvry.com/";
 } else {
@@ -16,14 +18,12 @@ export function parseUrl(url = window.location.href) {
   return parse(data);
 }
 
-export function login(
-  redirect_uri = window.location.origin + window.location.pathname
-) {
+export function login(redirect_uri = default_redirect_uri) {
   const params = { redirect_uri };
   cookie.remove(key);
   window.location.href = `${casUrl}?${stringify(params)}`;
 }
-export function logout() {
+export function logout(redirect_uri = default_redirect_uri) {
   cookie.remove(key);
   const params = { redirect_uri };
   window.location.href = `${casUrl}logout?${stringify(params)}`;
